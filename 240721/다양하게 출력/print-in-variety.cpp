d95b42b30200a33898ff6f0ed1d2c7dc63eda0b8
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-07-21 15:50:46 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/다양하게 출력/print-in-variety.cpp
+++ 240721/다양하게 출력/print-in-variety.cpp
@@ -5,6 +5,6 @@ int main() {
     cout << "Total days in Year\n";
     cout << 365 << "\n";
     cout << "Circumference rate\n";
-    cout << 3.1415926535 << "\n";
+    cout << "3.1415926535";
     return 0;
 }
